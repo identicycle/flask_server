455a2a852fcb42ce297c5959daa0b6e44cf483b1
Lisa Nam <lsyn2016@gmail.com>
2024-04-02 12:04:02 -0400

    inner node visualization

--- server.py
+++ server.py
@@ -35,6 +35,9 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+# Image Selected to be used for the inner node visualization
+image_selected = [[2,3,4,6,8,10,12,14],[2,4,6,9,12,14,15,20,21,24,26,27,28,29,32,33],[2,3,7,9,10,12,13,18,19,25,26,27,29,30,31,32,33,35,38,41,42,46,50,51,53,54,55,56,58,60,62,64,65,68,69,501]]
+
 @app.route('/')  # When someone goes to / on the server, execute the following function
 def home():
   return app.send_static_file('index.html')  # Return index.html from the static folder
@@ -93,7 +96,7 @@ def image_upload_view():
         'image_name': img_name,
         'image_filename': img_filename,
         'image_path': image_path,
-        'image_selected': [[2,3,4,6,8,10,12,14],[2,4,6,9,12,14,15,20,21,24,26,27,28,29,32,33],[2,3,7,9,10,12,13,18,19,25,26,27,29,30,31,32,33,35,38,41,42,46,50,51,53,54,55,56,58,60,62,64,65,68,69,501]],
+        'image_selected': image_selected,
         'predicted_class': custom_image_pred_class,
         'predicted_probs': {class_names[i]: round(probabilities[i].item(),4) for i in range(len(class_names))}
     }
@@ -114,6 +117,7 @@ def get_Image_Filters(img,name,raw_img):
   for i, maps in enumerate(activation_maps):
     num_filters = maps.size(1)  # Number of filters
     for j in range(num_filters):
+      if j in image_selected[i]: # to save only images needed
         filter_path = os.path.join(output_dir, f'{name}_{i+1}_{j}_image.png')
         filter_image = maps[0, j].detach().cpu().numpy()
         plt.imsave(filter_path, filter_image)
