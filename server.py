f4c4e8caf33b2bf74c58837a106f65e171f76562
Lisa Nam <lsyn2016@gmail.com>
2024-04-02 12:13:50 -0400

    server ready for production

--- server.py
+++ server.py
@@ -123,7 +123,6 @@ def get_Image_Filters(img,name,raw_img):
         plt.imsave(filter_path, filter_image)
 if __name__ == '__main__':  # If the script that was run is this script (we have not been imported)
   if os.environ.get('ENV')=="production":
-    from waitress import serve
-    serve(app, host='0.0.0.0', port=80)
+    app.run(host="localhost", port=80)
   else:
-    app.run(host="localhost", port=8080, debug=True)  # Start the server
+    app.run(host="localhost", port=8080, debug=True)
